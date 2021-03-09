268f5609fcb4ea0e1a295fe2f814862768794c6d
David Wandratsch <david.wandratsch@htl-wels.at>
2021-03-09 07:57:01 +0100

    initial commit

---
+++ src/main/TheMain.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import viewcontroller.PhonebookC;
+
+import java.io.File;
+
+public class TheMain extends Application
+{
+  @Override
+  public void start(Stage primaryStage)
+  {
+    File db = new File("phonebook.csv");
+    PhonebookC.show(primaryStage, db);
+  }
+  
+  public static void main(String[] args)
+  {
+    //launch the JavaFX Application
+    launch(args);
+  }
+}
